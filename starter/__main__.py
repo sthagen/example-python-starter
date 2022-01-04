141273b10f4b288aa75711c05a9449bc26813d31
Stefan Hagen <stefan@hagen.link>
2022-01-04 19:04:11 +0100

    Fixed names

--- starter/__main__.py
+++ starter/__main__.py
@@ -2,7 +2,7 @@
 # pylint: disable=expression-not-assigned,line-too-long,missing-module-docstring
 import sys
 
-from starter.cli import cli
+from starter.cli import main
 
 if __name__ == '__main__':
-    sys.exit(cli.main(argv=sys.argv[1:]))  # pragma: no cover
+    sys.exit(main(argv=sys.argv[1:]))  # pragma: no cover
